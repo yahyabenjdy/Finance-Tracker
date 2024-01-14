28e2b10d957eede970e5d7dc99a1cf7db1f09b29
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2024-01-14 19:42:08 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-// page & components
+// pages & components
 import Home from "./pages/home/Home";
-import Login from "./pages/login/login";
+import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/login">
